8664c371c1a67611c85d0d3b1dfd9ec4b882af5d
vini-torres <vinicius.carlosoficial@outlook.com>
2024-04-16 15:31:34 -0300

    refactor: changing structure of header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,9 @@
-import backgroundHeader from '../../assets/bg-header.svg'
+import logoTipo from '../../assets/logo.svg'
 
 export function Header() {
   return (
-    <header>
-      <img
-        src={backgroundHeader}
-        alt="background do Header com a logo do Github Blog"
-        className="max-h-72 w-full"
-      />
+    <header className="flex h-[18.5rem] w-full items-center justify-center bg-[url('/public/cover-header.svg')] bg-cover bg-no-repeat">
+      <img src={logoTipo} alt="Logo do Github Blog" />
     </header>
   )
 }
